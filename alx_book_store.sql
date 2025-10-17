eaa8ffc623cc54f410707d745116e9e3dbca3bab
Aicha <aichakelia01@gmail.com>
2025-10-17 15:32:39 +0100

    Add alx_book_store database schema

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,37 +1,37 @@
    CREATE DATABASE alx_book_store;
    USE alx_book_store;
-    CREATE TABLE AUTHORS (
+    CREATE TABLE Authors (
          author_id INT PRIMARY KEY,
          author_name VARCHAR(215)
     );
-    CREATE TABLE BOOKS (
+    CREATE TABLE Books (
          book_id INT PRIMARY KEY,
          title VARCHAR(130),
          author_id INT,
-         FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id),
+         FOREIGN KEY (author_id) REFERENCES Authors(author_id),
          price DOUBLE ,
          publication_date DATE 
     );
 
-    CREATE TABLE CUSTOMERS (
+    CREATE TABLE Customers (
          customer_id INT PRIMARY KEY,
          customer_name VARCHAR(215),
          email VARCHAR(215),
          address TEXT
     );
 
-    CREATE TABLE ORDERS (
+    CREATE TABLE Orders (
             order_id INT PRIMARY KEY,
             customer_id INT,
-            FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id),
+            FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
             order_date DATE
     );
 
-    CREATE TABLE ORDER_DETAILS (
+    CREATE TABLE Order_Details(
         orderdetailid INT PRIMARY KEY,
         order_id INT,
         book_id INT,
-        FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-        FOREIGN KEY (book_id) REFERENCES BOOKS(book_id),
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+        FOREIGN KEY (book_id) REFERENCES Books(book_id),
         quantity DOUBLE
     );
